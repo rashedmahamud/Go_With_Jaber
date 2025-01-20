5787613588ed0f8b68a276c0823a045b4eeef44a
Rashed <rashedmahamud93@gmail.com>
2025-01-20 19:39:30 +0600

    text file added

--- DB_Project/obj/Debug/net8.0/DB_Project.AssemblyInfo.cs
+++ DB_Project/obj/Debug/net8.0/DB_Project.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DB_Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ae098ab20bed645d3103c17a7ab49ab656b7b782")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5ba06498989834cb51d8ca4902a2db233f935ad7")]
 [assembly: System.Reflection.AssemblyProductAttribute("DB_Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DB_Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
